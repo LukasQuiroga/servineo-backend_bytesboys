1e160a5deb1eef3c15d0570f1a981b8fd5d635f6
milton Ayca <miltonaycacolque1@gmail.com>
2025-11-15 21:15:12 -0400

    config: agregar variables de entorno para notificaciones para gmail

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -9,4 +9,10 @@ export const ENV = {
   MONGO_PASS: process.env.MONGO_PASS,
   MONGO_HOST: process.env.MONGO_HOST,
   MONGO_DB: process.env.MONGO_DB,
+  SMTP_HOST: process.env.SMTP_HOST,
+  SMTP_PORT: Number(process.env.SMTP_PORT), // Convertimos a número
+  SMTP_SECURE: process.env.SMTP_SECURE === 'true', // Convertimos a booleano
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASSWORD: process.env.SMTP_PASSWORD,
+  SMTP_FROM: process.env.SMTP_FROM,
 };
